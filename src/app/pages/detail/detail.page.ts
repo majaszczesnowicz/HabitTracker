cc1d6da2b259a50afc4182e9a448e94b41bb9882
Maja Szczęsnowicz <ma.szczesnowicz@gmail.com>
2020-11-03 22:16:04 +0100

    usuwanie nawyków

--- src/app/pages/detail/detail.page.ts
+++ src/app/pages/detail/detail.page.ts
@@ -3,7 +3,7 @@ import { NavController, NavParams } from '@ionic/angular';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { ActivatedRoute, Router } from '@angular/router';
-
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-detail',
@@ -15,12 +15,36 @@ export class DetailPage implements OnInit {
   uid = {};
 
   constructor(public navCtrl: NavController, private afAuth: AngularFireAuth, private db: AngularFirestore, 
-    private router: Router, private route: ActivatedRoute, public navParams: NavParams) { 
+    private router: Router, private route: ActivatedRoute, public navParams: NavParams, private alertCtrl: AlertController) { 
       this.afAuth.authState.subscribe(user => {
         if (user)
           this.uid = user.uid;
       });
       
+  }
+
+  async del(habitId){
+    const alert = await this.alertCtrl.create({
+      header: 'czy na pewno chcesz usunąć ten nawyk?',
+      buttons: [
+      {
+        text: 'anuluj',
+        role: 'cancel',
+        handler: () => {
+          console.log('anuluj');
+        }
+      },
+      {
+        text: 'usuń',
+        handler: () => {
+          console.log('usuń');
+          this.db.doc(`users/${this.uid}/trwajace/${habitId}`).delete();
+          this.router.navigateByUrl(`/`);
+        }
+      }
+      ],
+    });
+    return await alert.present();
   }
 
   ngOnInit() {
