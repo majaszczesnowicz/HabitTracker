2cbb1a7d2352c6046619122a832c1fe0061ad86a
Paulina <Plpalinka6@gmail.com>
2020-11-18 14:11:27 +0100

    Add privacy.

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -5,11 +5,9 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
+
 export class LoginPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
